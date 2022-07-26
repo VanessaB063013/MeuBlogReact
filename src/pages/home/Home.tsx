41c97699df5ce0700bc37839104475afe469a0a3
Vanessa Basilio Jardim <vanessajardim063013@gmail.com>
2022-07-26 19:21:25 -0300

    React no meu-blog

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css";
+
+function Home(){
+    return(
+
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img className='img' src='https://miro.medium.com/max/1400/1*TMYPt-WhRNKUTdXXQ0N2SQ.jpeg' alt="Imagem Tela inicial"/>
+        
+        </>
+    );
+}
+
+export default Home;
